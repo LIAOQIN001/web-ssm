8059f3ac1ddc88c89ac573f70ae3f432a350dd39
liao <791292544@qq.com>
2017-10-16 21:54:39 +0800

    gfhdrth

---
+++ src/main/java/ssm/model/User.java
@@ -0,0 +1,34 @@
+package ssm.model;
+
+/**
+ * Created by liao on 2017/10/12.
+ */
+public class User {
+    private String id;
+    private String name;
+    private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
